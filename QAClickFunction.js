af1f116003bbdbdc0e84e627370cdd351327d7a8
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-08-31 11:46:06 +0530

    Update QAClickFunction.js

--- QAClickFunction.js
+++ QAClickFunction.js
@@ -1,4 +1,5 @@
 var XLSX = require('xlsx');
+
 var workbook = XLSX.readFile('../Users/SandipNandi/Videos/QAClickAcedamy_Data.xlsx');
 var Worksheet1 = workbook.Sheets['Sheet1'];
 var Worksheet2 = workbook.Sheets['Sheet2'];
