75ba65d57eac2766b5b26ac8a0f88ac55552b7ed
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-03 20:59:52 +0530

    Update Garbage.js

--- Garbage.js
+++ Garbage.js
@@ -6,7 +6,6 @@ it('Go inside Check out->Validate the Total Amount->Purchase by selecting countr
          var rr = text4.split(' ');
          var rrs = parseInt(rr[1]);
          console.log('rrs:' + rrs);
-
          add1(rrs)
 
      })
