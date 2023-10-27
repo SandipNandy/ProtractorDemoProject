f6468686e6220a06da9e96ec4650409bf499a4be
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-10-27 09:02:21 +0530

    Update QAClickMain.js

--- QAClickMain.js
+++ QAClickMain.js
@@ -4,6 +4,7 @@ var XLSX = require('xlsx');
 var workbook = XLSX.readFile('../Users/SandipNandi/Videos/QAClickAcedamy_Data.xlsx');
 
 var QAClickAcademy_POM = require('./QAClickPom.js');
+
 var QAClickAcademy_Fun = require('./QAClickFunction.js');
 const { getMaxListeners } = require('process');
 var Worksheet1 = workbook.Sheets['Sheet1'];
