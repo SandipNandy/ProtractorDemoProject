d1e7861303856dba1378ff979c487669fbe34ca6
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2025-03-26 00:25:04 +0530

    Update QAClickPom.js

--- QAClickPom.js
+++ QAClickPom.js
@@ -16,7 +16,6 @@ let QAClickAcedamyPom = function () {
      
  };
  let ErrorMessageUsernameEmail=element.all(by.css("[class='alert alert-danger']"));
-    
  this.Error_Message_Username=function(){
      
     return ErrorMessageUsernameEmail;
