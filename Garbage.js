53829d6eb0cb42d2634590e9c9454c5c7b5e52a7
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-05-19 09:15:39 +0530

    Update Garbage.js

--- Garbage.js
+++ Garbage.js
@@ -2,7 +2,6 @@ it('Go inside Check out->Validate the Total Amount->Purchase by selecting countr
     
     //element(by.css("li[class='nav-item active']")).click();
     QAClickAcademy_POM.Click_On_Checkout();
-    
     /* element(by.xpath("//tr[1]//td[4]//strong[1]")).getText().then(function (text4) {
          console.log(text4);
          var rr = text4.split(' ');
